9979391bd4f407cebc7cf23c7df9a718920eb172
veronabaudracco <veronabaudracco@gmail.com>
2021-10-07 16:57:04 -0300

    finished first exercise

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,18 @@
+// a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+var number1 = 12, number2 = 25, sum
+
+sum = number1 + number2
+console.log(sum)
+// b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+var word1 = 'Mi nombre es ', word2 = 'Verona', phrase
+
+phrase = word1 + word2
+console.log(phrase)
+/* c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+guardando el resultado de la suma en una 3er variable (utilizar length).*/
+var word1 = 'radium', word2 = 'rocket', numberOfLetters1, numberOfLetters2, totalamount
+
+numberOfLetters1 = word1.length
+numberOfLetters2 = word2.length
+totalamount = numberOfLetters1 + numberOfLetters2
+console.log(totalamount)
